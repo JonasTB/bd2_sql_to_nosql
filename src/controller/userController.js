fa22277c9748df807d6c2a8508d743cf74cf48d3
EazyyPeasyy <gilsonbezerrab10@gmail.com>
2022-06-05 18:53:12 -0300

    add new funcs

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,24 +2,35 @@ const Redis = require('../database/redisConfig');
 
 module.exports = {
     create: async (req, res) => {
-        const { id, nome, logradouro, numero, complemento, bairro, cidade, uf, cep, data_nascimento } = req.body;
+        // const { id, nome, logradouro, numero, complemento, bairro, cidade, uf, cep, data_nascimento } = req.body;
+        const { id } = req.body;
         try {
-            const user = {
-                "nome": `${nome}`,
-                "logradouro": `${logradouro}`,
-                "numero": `${numero}`,
-                "complemento": `${complemento}`,
-                "bairro": `${bairro}`,
-                "cidade": `${cidade}`,
-                "uf": `${uf}`,
-                "cep": `${cep}`,
-                "data_nascimento": `${data_nascimento}`
-            }
-            const create = await Redis.setRedis(`id-${id}`, JSON.stringify(user));
+            // const user = {
+            //     "nome": `${nome}`,
+            //     "logradouro": `${logradouro}`,
+            //     "numero": `${numero}`,
+            //     "complemento": `${complemento}`,
+            //     "bairro": `${bairro}`,
+            //     "cidade": `${cidade}`,
+            //     "uf": `${uf}`,
+            //     "cep": `${cep}`,
+            //     "data_nascimento": `${data_nascimento}`
+            // }
+            const create = await Redis.setRedis(id, JSON.stringify(req.body));
 
             return res.status(201).json(create);
         } catch (err) {
             console.log(err);
+            return res.status(400).json(err)
+        }
+    },
+    getKey: async(req, res) => {
+        const { id } = req.query;
+        try {
+            const key = JSON.parse(await Redis.getRedis(id));
+            console.log(key.nome)
+            return res.status(200).json(key);
+        } catch (err) {
             return res.status(400).json(err)
         }
     }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const controller = require('../controller/userController');
 
-router.post('/', controller.create);
+router.post('/create', controller.create);
+router.get('/search_key', controller.getKey);
+// router.get('/search_attribute', controller.)
 
 module.exports = router;
